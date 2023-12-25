1dc86e0be51605423c104cea216c7f8bcdf4e7a2
Ravina Gaikawad <ravinagaikawad1995@gmail.com>
2023-12-25 17:58:17 -0600

    Create Leetcode_217_Contains_duplicate.py

---
+++ Leetcode_217_Contains_duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        nums_set = set()
+
+        for num in nums:
+            if num in nums_set:
+                return True
+
+            nums_set.add(num)
+
+        return False
+        
