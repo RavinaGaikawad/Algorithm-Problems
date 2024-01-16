07c99ed203c364d97f37488792583d07b06de01c
Ravina Gaikawad <ravina.gaikawad@edf-re.com>
2024-01-15 19:59:28 -0600

    Adding new file

---
+++ Leetcode_242_Valid_anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_dict = {}
+        t_dict = {}
+
+        for i in range(len(s)):
+            s_dict[s[i]] = s_dict.get(s[i], 0) + 1
+            t_dict[t[i]] = t_dict.get(t[i], 0) + 1
+
+        return s_dict == t_dict
+
+'''
+ T = O(N)
+ S = O(N)
+'''
